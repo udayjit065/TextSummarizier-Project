302cf5c46f860bdccdad5b9be333156a32351c13
udayjit <udayjit065@gmail.com>
2024-11-24 19:40:15 +0530

    files

---
+++ template.py
@@ -0,0 +1,49 @@
+import os
+from pathlib import Path
+import logging
+
+# Fixing the logging configuration
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
+
+project_name = 'textSummarizaton'
+project_path = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trails.ipynb",
+    "test.py",
+    
+]
+
+for filePath in project_path:
+    # Convert filePath string to a Path object
+    filepath = Path(filePath)
+    filedir, filename = os.path.split(filePath)
+    
+    # Create directories if they don't exist
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file {filename}")
+
+    # Create empty files if they don't exist or are empty
+    if not filepath.exists() or filepath.stat().st_size == 0:
+        with open(filepath, 'w') as f:
+            pass
+        logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"{filename} already exists")
